93faacd7da5c4c1e686a0ddbe40f082c4f34b26f
wwaenigrj0902 <314981304@qq.com>
2020-11-05 16:54:28 +0800

    王伟spring boot idea  git

---
+++ src/main/java/com/example/ww/example/WwApplication.java
@@ -0,0 +1,15 @@
+package com.example.ww.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WwApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/ww/WwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ww;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
